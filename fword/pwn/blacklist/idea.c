0eef2bbe23a5ea57c76445c5d0ddd7b26055b841
BigB00st <botzer.2002@gmail.com>
2020-08-30 21:14:05 +0300

    modified idea.c

--- fword/pwn/blacklist/idea.c
+++ fword/pwn/blacklist/idea.c
@@ -5,10 +5,11 @@
 #include <sys/sendfile.h>
 #include <unistd.h>
 
-#define RW_ADDR 0xdeadbeef
+#define RW_ADDR 0xdeadbeef // actually some bss address in exploit
 
-int main(){
-    read(0, RW_ADDR, 900);
-    int fd = openat(0, "/path/to/flag.txt",O_RDONLY); //0, 0
+int main() {
+    read(0, RW_ADDR, 900); // write path of flag to address
+    int fd = openat(0, "/path/to/flag.txt", O_RDONLY);
     sendfile(1, fd, 0, 0xffff);
-return 0;}
+    return 0;
+}
